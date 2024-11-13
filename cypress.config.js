a19555c7c8c8d0590f9be802c2309a81aded18f3
Yuliya Banko <yuliyabanko@gmail.com>
2024-11-13 10:29:23 +0300

    HW_6.1.1

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
-    baseUrl: "https://staging.lpitko.ru",
+    baseUrl: "https://santa-secret.ru",
     testIsolation: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -8,37 +8,30 @@ const inviteeDashboardPage = require("../fixtures/pages/inviteeDashboardPage.jso
 import { faker } from "@faker-js/faker";
 
 describe("user can create a box and run it", () => {
-  //пользователь 1 логинится
-  //пользователь 1 создает коробку
-  //пользователь 1 получает приглашение
-  //пользователь 2 переходит по приглашению
-  //пользователь 2 заполняет анкету
-  //пользователь 3 переходит по приглашению
-  //пользователь 3 заполняет анкету
-  //пользователь 4 переходит по приглашению
-  //пользователь 4 заполняет анкету
-  //пользователь 1 логинится
-  //пользователь 1 запускает жеребьевку
+
   let newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
   let wishes = faker.word.noun() + faker.word.adverb() + faker.word.adjective();
   let maxAmount = 50;
+  let minAmount = 10;
   let currency = "Евро";
   let inviteLink;
 
   it("user logins and create a box", () => {
     cy.visit("/login");
     cy.login(users.userAutor.email, users.userAutor.password);
-    cy.contains("Создать коробку").click();
+    cy.contains("Создать коробку").should('be.visible').click({ force: true });
     cy.get(boxPage.boxNameField).type(newBoxName);
     cy.get(generalElements.arrowRight).click();
     cy.get(boxPage.sixthIcon).click();
     cy.get(generalElements.arrowRight).click();
-    cy.get(boxPage.giftPriceToggle).check({ force: true });
-    cy.get(boxPage.maxAnount).type(maxAmount);
+    cy.get(boxPage.giftPriceToggle).click({ force: true });
+    cy.get(boxPage.minAmount).type(minAmount);
+    cy.get(boxPage.maxAmount).type(maxAmount);
     cy.get(boxPage.currency).select(currency);
-    cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
+    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({ force: true });
+   
     cy.get(dashboardPage.createdBoxName).should("have.text", newBoxName);
     cy.get(".layout-1__header-wrapper-fixed .toggle-menu-item span")
       .invoke("text")
@@ -87,10 +80,14 @@ describe("user can create a box and run it", () => {
     cy.get(
       ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
     ).click();
+    cy.get('.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med')
+  .should('exist')
+  .click();
+    cy.contains(newBoxName).parent().find('a.base--clickable').click();
+    cy.get(".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner")
+      .click();
     cy.contains("Архивация и удаление").click({ force: true });
-    cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
-      "Удалить коробку"
-    );
-    cy.get(".btn-service").click();
+    cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type("Удалить коробку");
+    cy.get('.layout-row-end > .btn-service').click();
   });
 });
