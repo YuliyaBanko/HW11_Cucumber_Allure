e7f652091f16eb53c8a183511a23d1ea105c8d05
Yuliya Banko <yuliyabanko@gmail.com>
2024-12-18 16:30:34 +0300

    HW11 added allure scripts

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
@@ -14,10 +14,17 @@ module.exports = defineConfig({
       on("file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin.default(config)],
-        }));
-        preprocessor.addCucumberPreprocessorPlugin(on, config);
-        return config;
+        })
+      );
+
+      preprocessor.addCucumberPreprocessorPlugin(on, config);
+      allureWriter(on, config);
+      return config; 
     }, 
-    specPattern: "**/*.feature",
+    env: {
+      allureReuseAfterSpec: true, 
+    },
+    specPattern: "**/*.feature", 
   },
 });
+
